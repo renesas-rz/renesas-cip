ce938d1d3501529fb14fc99dc6b8bdf003d07a73
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-11-22 11:02:22 +0900

    mmc: tmio: Add internal DMA transfer end
    Although the completion of the data transfer processing was judged only
    by the Access End interrupt of SDHI, the Transfer End interruption of
    DMAC was also added to the condition.
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
    Signed-off-by: Takeshi Saito <takeshi.saito.xv@renesas.com>
    Signed-off-by: Huynh Thanh Hung <hung.huynh.wz@rvc.renesas.com>

--- drivers/mmc/host/tmio_mmc.h
+++ drivers/mmc/host/tmio_mmc.h
@@ -111,6 +111,12 @@
 		TMIO_STAT_CARD_REMOVE | TMIO_STAT_CARD_INSERT)
 #define TMIO_MASK_IRQ     (TMIO_MASK_READOP | TMIO_MASK_WRITEOP | TMIO_MASK_CMD)
 
+#define TMIO_TRANSTATE_DEND	0x00000001
+#define TMIO_TRANSTATE_AEND	0x00000002
+
+/* Check LSI revisions and set specific quirk value */
+#define DTRAEND1_SET_BIT17	BIT(0)
+
 struct tmio_mmc_data;
 struct tmio_mmc_host;
 
@@ -122,6 +128,7 @@ struct tmio_mmc_dma_ops {
 	void (*release)(struct tmio_mmc_host *host);
 	void (*abort)(struct tmio_mmc_host *host);
 	void (*dataend)(struct tmio_mmc_host *host);
+	bool (*dma_irq)(struct tmio_mmc_host *host);
 };
 
 struct tmio_mmc_host {
@@ -146,6 +153,8 @@ struct tmio_mmc_host {
 	struct platform_device *pdev;
 	struct tmio_mmc_data *pdata;
 
+	u32			sdhi_quirks;
+
 	/* DMA support */
 	bool			force_pio;
 	struct dma_chan		*chan_rx;
@@ -153,6 +162,7 @@ struct tmio_mmc_host {
 	struct tasklet_struct	dma_issue;
 	struct scatterlist	bounce_sg;
 	u8			*bounce_buf;
+	u32			dma_tranend1;
 
 	/* Track lost interrupts */
 	struct delayed_work	delayed_reset_work;
@@ -161,6 +171,7 @@ struct tmio_mmc_host {
 	/* Cache */
 	u32			sdcard_irq_mask;
 	u32			sdio_irq_mask;
+	u32			dma_irq_mask;
 	unsigned int		clk_cache;
 
 	spinlock_t		lock;		/* protect host private data */
@@ -168,6 +179,9 @@ struct tmio_mmc_host {
 	struct mutex		ios_lock;	/* protect set_ios() context */
 	bool			native_hotplug;
 	bool			sdio_irq_enabled;
+
+	spinlock_t		trans_lock;	/* protect trans_state */
+	unsigned int		trans_state;
 
 	/* Mandatory callback */
 	int (*clk_enable)(struct tmio_mmc_host *host);
@@ -208,6 +222,9 @@ void tmio_mmc_host_free(struct tmio_mmc_host *host);
 int tmio_mmc_host_probe(struct tmio_mmc_host *host);
 void tmio_mmc_host_remove(struct tmio_mmc_host *host);
 void tmio_mmc_do_data_irq(struct tmio_mmc_host *host);
+
+void tmio_mmc_set_transtate(struct tmio_mmc_host *host, unsigned int state);
+void tmio_mmc_clear_transtate(struct tmio_mmc_host *host);
 
 void tmio_mmc_enable_mmc_irqs(struct tmio_mmc_host *host, u32 i);
 void tmio_mmc_disable_mmc_irqs(struct tmio_mmc_host *host, u32 i);
