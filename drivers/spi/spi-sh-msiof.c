f0229c7ff5352035607d26e257f2a578b0361dbf
khanhluu <khanh.luu.xw@rvc.renesas.com>
2017-12-15 23:27:23 +0700

    spi: sh-msiof: add support for r8a77470
    Signed-off-by: khanhluu <khanh.luu.xw@rvc.renesas.com>

--- drivers/spi/spi-sh-msiof.c
+++ drivers/spi/spi-sh-msiof.c
@@ -983,6 +983,7 @@ static const struct of_device_id sh_msiof_match[] = {
 	{ .compatible = "renesas,msiof-r8a7793",   .data = &r8a779x_data },
 	{ .compatible = "renesas,msiof-r8a7794",   .data = &r8a779x_data },
 	{ .compatible = "renesas,msiof-r8a7745",   .data = &r8a779x_data },
+	{ .compatible = "renesas,msiof-r8a77470",   .data = &r8a779x_data },
 	{},
 };
 MODULE_DEVICE_TABLE(of, sh_msiof_match);
