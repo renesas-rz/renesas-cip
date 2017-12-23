a76064062b7b4b89ed971da449fc0d5f00ab9439
vietn <vietn@fsoft.com.vn>
2017-12-24 02:20:11 +0700

    da9063: using RTC to reboot silk board
    SILK do have "wake" button. By default on Silk, reboot
    switches off PMIC leading to halted state. The configuration
    of PMIC allows only buttons or RTC wakeup, which will
    get board out of switched-off state. This patch
    uses RTC to reboot the board.
    
    Signed-off-by: vietn <vietn@fsoft.com.vn>
    Signed-off-by: Nguyen Van Linh [FGA.BU13] <LinhNV25@fsoft.com.vn>
    Signed-off-by: vietn <vietn@fsoft.com.vn>

--- drivers/watchdog/da9063_wdt.c
+++ drivers/watchdog/da9063_wdt.c
@@ -22,6 +22,8 @@
 #include <linux/mfd/da9063/core.h>
 #include <linux/reboot.h>
 #include <linux/regmap.h>
+#include <linux/of.h>
+#include <linux/io.h>
 
 /*
  * Watchdog selector to timeout in seconds.
@@ -129,8 +131,14 @@ static int da9063_wdt_restart_handler(struct notifier_block *this,
 						   restart_handler);
 	int ret;
 
+#ifdef CONFIG_RTC_DA9063_RESET_RENESAS_SILK
+	/* To be sure RTC interrupt triggered */
+	mdelay(1000);
+#endif
+
 	ret = regmap_write(wdt->da9063->regmap, DA9063_REG_CONTROL_F,
 			   DA9063_SHUTDOWN);
+
 	if (ret)
 		dev_alert(wdt->da9063->dev, "Failed to shutdown (err = %d)\n",
 			  ret);
