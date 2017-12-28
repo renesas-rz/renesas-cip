renesas-rz/renesas-cip

commit 587eb8d94ef51ba6153ec590432da81088b750fd

rcar-du: Work around to enable iwg20m LCD display
On iwg20m, when there is no Daughterboard, DU cannot
initialize encoder for HDMI.
However, the return code make kernel retry again
many times, instead of continue initialize next output.
This makes the LCD display not usable.

This patch works around this by changing the return
code, thus prevent retry.

Signed-off-by: Hung Tran <hung.tran.jy@renesas.com>

drivers/gpu/drm/rcar-du/rcar_du_encoder.c
@@ -201,5 +201,11 @@ done:
 		devm_kfree(rcdu->dev, renc);
 	}
 
+	if (ret == -EPROBE_DEFER)
+		/* Return -EPROBE_DEFER will make kernel trying to initialize
+		 * current output again. But in certain case it causes looping
+		 * and kernel cannot init another output. Disable temporarily */
+		ret = -1;
+
 	return ret;
 }

commit b2f1c93cdb23952cdaaae7166d11848aadbb685e

drm: rcar-du: Fix no-retry issue on iwg23s
Issue caused by workaround in commit 587eb8d94ef5 ("rcar-du: Work
around to enable iwg20m LCD display")
- The mentioned commit prevents DU from re-initializing HDMI encoder on
  iwg23s board
- This new commit limits the workaround to work on iwg20m and iwg22m boards
  only

Signed-off-by: Hung Dong <hung.dong.xd@rvc.renesas.com>

drivers/gpu/drm/rcar-du/rcar_du_encoder.c
@@ -206,7 +206,9 @@ done:
 		devm_kfree(rcdu->dev, renc);
 	}
 
-	if (ret == -EPROBE_DEFER)
+	if (ret == -EPROBE_DEFER &&
+			(of_machine_is_compatible("iw,iwg20m") ||
+			of_machine_is_compatible("iw,iwg22m")))
 		/* Return -EPROBE_DEFER will make kernel trying to initialize
 		 * current output again. But in certain case it causes looping
 		 * and kernel cannot init another output. Disable temporarily */