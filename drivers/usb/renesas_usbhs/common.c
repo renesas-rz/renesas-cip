3e3134229fd197b5249bb94d3391e037ed7b6be0
Kha Tran <kha.tran.px@rvc.renesas.com>
2018-01-23 13:16:37 +0700

    usb: renesas_usbhs: Add support for r8a7744
    This driver supports other SoCs, but they need boards/Soc depend code.
    So, this patch adds device tree support for r8a7744 initially.
    
    Signed-off-by: thongsyho <thong.ho.px@rvc.renesas.com>
    Signed-off-by: Kha Tran <kha.tran.px@rvc.renesas.com>

--- drivers/usb/renesas_usbhs/common.c
+++ drivers/usb/renesas_usbhs/common.c
@@ -477,6 +477,10 @@ static const struct of_device_id usbhs_of_match[] = {
 		.data = (void *)USBHS_TYPE_RCAR_GEN2,
 	},
 	{
+		.compatible = "renesas,usbhs-r8a7744",
+		.data = (void *)USBHS_TYPE_RCAR_GEN2,
+	},
+	{
 		.compatible = "renesas,usbhs-r8a7794",
 		.data = (void *)USBHS_TYPE_RCAR_GEN2,
 	},
