f9ae258c777c279ffc91a3bef6ba6e98fdbb9d7f
hienhuynh <hien.huynh.px@rvc.renenas.com>
2018-06-05 09:43:44 +0700

    Revert "serial: sh-sci: Fix repeats of previous printed messages in rebooting"
    This reverts commit <19fb7a990d46b2595c5c968740e85f3fd0f3666f>.
    
    Revert this commit due to in CIP version, struct sci_port does not have element "sg_tx"
    
    Signed-off-by: Tung Vo <tung.vo.eb@rvc.renesas.com>
    Signed-off-by: Nguyen Van Linh [FGA.BU13] <LinhNV25@fsoft.com.vn>
    Signed-off-by: vietn <vietn@fsoft.com.vn>
    Signed-off-by: hienhuynh <hien.huynh.px@rvc.renenas.com>

--- drivers/tty/serial/sh-sci.c
+++ drivers/tty/serial/sh-sci.c
@@ -116,7 +116,6 @@ struct sci_port {
 	struct work_struct		work_tx;
 	struct timer_list		rx_timer;
 	unsigned int			rx_timeout;
-	int                             circ_flush_flag;
 #endif
 
 	bool autorts;
@@ -1051,11 +1050,10 @@ static void sci_dma_tx_complete(void *arg)
 
 	spin_lock_irqsave(&port->lock, flags);
 
-	if (s->circ_flush_flag == SCI_CIRC_FLSH_OFF) {
-		xmit->tail += sg_dma_len(&s->sg_tx);
-		mit->tail &= UART_XMIT_SIZE - 1;
-		port->icount.tx += sg_dma_len(&s->sg_tx);
-	}
+	xmit->tail += s->tx_dma_len;
+	xmit->tail &= UART_XMIT_SIZE - 1;
+
+	port->icount.tx += s->tx_dma_len;
 
 	if (uart_circ_chars_pending(xmit) < WAKEUP_CHARS)
 		uart_write_wakeup(port);
@@ -1256,7 +1254,6 @@ static void work_fn_tx(struct work_struct *work)
 	 * consistent xmit buffer state.
 	 */
 	spin_lock_irq(&port->lock);
-	s->circ_flush_flag = SCI_CIRC_FLSH_OFF;
 	buf = s->tx_dma_addr + (xmit->tail & (UART_XMIT_SIZE - 1));
 	s->tx_dma_len = min_t(unsigned int,
 		CIRC_CNT(xmit->head, xmit->tail, UART_XMIT_SIZE),
@@ -2100,15 +2097,6 @@ static void sci_baud_calc_hscif(unsigned int bps, unsigned long freq,
 	}
 }
 
-static void sci_flush_buffer(struct uart_port *port)
-{
-#ifdef CONFIG_SERIAL_SH_SCI_DMA
-	struct sci_port *s = to_sci_port(port);
-
-	s->circ_flush_flag = SCI_CIRC_FLSH_ON;
-#endif
-}
-
 static void sci_reset(struct uart_port *port)
 {
 	const struct plat_sci_reg *reg;
@@ -2406,7 +2394,6 @@ static struct uart_ops sci_uart_ops = {
 	.break_ctl	= sci_break_ctl,
 	.startup	= sci_startup,
 	.shutdown	= sci_shutdown,
-	.flush_buffer	= sci_flush_buffer,
 	.set_termios	= sci_set_termios,
 	.pm		= sci_pm,
 	.type		= sci_type,
