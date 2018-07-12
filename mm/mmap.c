17fa1ec13b77b8ef9f3b7132a439e5962697d3fb
Binh Nguyen <binh.nguyen.uw@rvc.renesas.com>
2018-07-12 17:21:57 +0700

    Revert "mmap: relax file size limit for regular files"
    This reverts commit 6ea1dc96a03a ("mmap: relax file size limit
    	for regular files")
    
    Signed-off-by: Thao M. Hoang <thaohm2@fsoft.com.vn>
    Signed-off-by: Binh Nguyen <binh.nguyen.uw@rvc.renesas.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1290,7 +1290,7 @@ static inline int mlock_future_check(struct mm_struct *mm,
 static inline u64 file_mmap_size_max(struct file *file, struct inode *inode)
 {
 	if (S_ISREG(inode->i_mode))
-		return MAX_LFS_FILESIZE;
+		return inode->i_sb->s_maxbytes;
 
 	if (S_ISBLK(inode->i_mode))
 		return MAX_LFS_FILESIZE;
