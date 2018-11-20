2e3f35161401a333098769b30f577fc5e0f721db
khanhluu <khanh.luu.xw@rvc.renesas.com>
2018-11-20 14:00:36 +0700

    drm: rcar-du: Fix dpad signal routing for R8A7799X
    In the case of R8A7799X, the dpad0_source has an initial value of 0
    when starting kernel, DPAD can not be set with DU1 when using with
    LVDS0 -> HDMI (DU0). So correct it so that it searches and decides
    connector information of VGA.
    
    In addition, The default value of dpad0_source is determined to be 1,
    and the default setting is also changed.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>
    Signed-off-by: khanhluu <khanh.luu.xw@rvc.renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_drv.h
+++ drivers/gpu/drm/rcar-du/rcar_du_drv.h
@@ -30,6 +30,8 @@ struct rcar_du_device;
 #define RCAR_DU_FEATURE_CRTC_IRQ_CLOCK	(1 << 0)	/* Per-CRTC IRQ and clock */
 #define RCAR_DU_FEATURE_EXT_CTRL_REGS	(1 << 1)	/* Has extended control registers */
 #define RCAR_DU_FEATURE_VSP1_SOURCE	(1 << 2)	/* Has inputs from VSP1 */
+#define RCAR_DU_FEATURE_R8A7799X        (1 << 9)        /* Use R8A7799X */
+#define RCAR_DU_FEATURE_R8A77965_REGS (1 << 5) /* Use R8A77965 registers */
 
 #define RCAR_DU_QUIRK_ALIGN_128B	(1 << 0)	/* Align pitches to 128 bytes */
 
